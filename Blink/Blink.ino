0cff7bf883dc34d021eeea0bbb59d9c0ea26a587
Constantin <konstantin4all@gmail.com>
2019-03-05 16:13:07 +0200

    blink

---
+++ Blink/Blink.ino
@@ -0,0 +1,21 @@
+
+void setup() {
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+}
+
+
+void loop() {
+
+  digitalWrite(LED_BUILTIN, LOW);
+  
+  delay(1000);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+  delay(5000);
+
+}
