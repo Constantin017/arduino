86bd17918db558f610673feec7387409a7895db2
Constantin <konstantin4all@gmail.com>
2019-03-11 12:46:35 +0200

    recode, wiki, original

--- 74HC595/chip_74HC595/chip_74HC595.ino
+++ 74HC595/chip_74HC595/chip_74HC595.ino
@@ -1,29 +1,21 @@
-int _data = 2;
-int _clock = 5;
-int _latch = 4;
+int dataPin = 2;
+int clockPin = 5;
+int latchPin = 4;
 int ledState = 0;
 
 void setup()
 {
-  pinMode(_data, OUTPUT);
-  pinMode(_clock, OUTPUT);
-  pinMode(_latch, OUTPUT);
-  
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
 }
 
 void loop()
 {
 
-  delay(500);
-  digitalWrite(13, HIGH);
-  
-  digitalWrite(_latch, LOW);
-  shiftOut(_data, _clock, MSBFIRST, ledState);
-  digitalWrite(_latch, HIGH);
-  
-  delay(500);
-  digitalWrite(13, LOW);
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, ledState);
+  digitalWrite(latchPin, HIGH);
+  delay(1000);
   
 }
